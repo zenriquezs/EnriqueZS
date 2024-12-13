944b0e2bc381dc6226e7a89402cf7db7b7dd7533
Enrique Zuñiga Zuñiga <zenrizu@gmail.com>
2024-12-12 23:19:09 -0600

    Final  Web

--- assets/js/contactos.js
+++ assets/js/contactos.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
 
-// Configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyA-fCXhtsGA1mhwaX-t5JvRF3BMJ8OxmVs",
   authDomain: "portafolioweb-9b0b4.firebaseapp.com",
@@ -11,24 +10,16 @@ const firebaseConfig = {
   messagingSenderId: "48086854093",
   appId: "1:48086854093:web:0f3421a51b7d924cf97c84"
 };
-
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inicializar la base de datos
 const db = getDatabase(app);
-
-// Referencia al botón de envío del formulario
 document.getElementById("form").addEventListener("submit", function (e) {
   e.preventDefault();
 
-  // Obtener los valores del formulario
   const nombre = document.getElementById("nombre").value;
   const email = document.getElementById("email").value;
   const asunto = document.getElementById("asunto").value;
   const mensaje = document.getElementById("mensaje").value;
 
-  // Validaciones
   if (!nombre || !email || !asunto || !mensaje) {
     alert("Por favor, completa todos los campos antes de enviar el formulario.");
     return;
@@ -40,11 +31,8 @@ document.getElementById("form").addEventListener("submit", function (e) {
     alert("Por favor, introduce un correo electrónico válido.");
     return;
   }
-
-  // Reemplazar puntos en el correo para evitar problemas con las claves
   const safeEmail = email.replace(/\./g, "_");
 
-  // Guardar en Firebase Realtime Database
   set(ref(db, "contactos/" + safeEmail), {
     nombre: nombre,
     email: email,
@@ -53,7 +41,7 @@ document.getElementById("form").addEventListener("submit", function (e) {
   })
     .then(() => {
       alert("Formulario enviado con éxito!");
-      document.getElementById("form").reset(); // Limpiar el formulario
+      document.getElementById("form").reset();
     })
     .catch((error) => {
       console.error("Error al enviar el formulario:", error);
