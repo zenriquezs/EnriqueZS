78a362b48badff4c86b39acdcaaeecef6c5ee127
Enrique Zuñiga Zuñiga <zenrizu@gmail.com>
2025-07-26 01:31:13 -0600

    Actualizacion (Enrique ZS)

--- assets/js/image-configuration.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const images = document.querySelectorAll('img');
-    images.forEach(img => {
-        img.addEventListener('contextmenu', (e) => {
-            e.preventDefault();
-        });
-    });
-});
